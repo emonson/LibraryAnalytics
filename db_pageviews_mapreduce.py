db60bc392351cadbda8e9925ced9b7273d8223eb
Eric E Monson <emonson@cs.duke.edu>
2015-02-23 08:48:43 -0500

    slight connection updates

--- db_pageviews_mapreduce.py
+++ db_pageviews_mapreduce.py
@@ -13,8 +13,8 @@ from bson.son import SON
 
 # Make a connection to Mongo.
 try:
-    # db_conn = Connection()
-    db_conn = Connection("emo2.trinity.duke.edu", 27017)
+    db_conn = Connection()
+    # db_conn = Connection("emo2.trinity.duke.edu", 27017)
 except ConnectionFailure:
     print "couldn't connect: be sure that Mongo is running on localhost:27017"
     sys.exit(1)
