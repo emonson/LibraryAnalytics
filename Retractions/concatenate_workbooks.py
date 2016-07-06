7e4ce45d6cfdbba359a386631592d4416e32128d
Eric E Monson <emonson@cs.duke.edu>
2016-07-06 14:58:00 -0400

    improved concatenation script. ordering fields by how many databases include them

--- Retractions/concatenate_workbooks.py
+++ Retractions/concatenate_workbooks.py
@@ -7,7 +7,7 @@ import xlrd
 
 data_dir = '/Users/emonson/Dropbox/People/CiaraHealy'
 in_file = 'Retractions_20160629.xlsx'
-out_file = 'Retractions_concat_20160705.txt'
+out_file_base = 'Retractions_concat_20160705'
 
 list_dfs = []
 in_path = os.path.join(data_dir, in_file)
@@ -32,13 +32,31 @@ for df in list_dfs:
 
 # print sorted([(k,len(v)) for k,v in fields.items()], key=lambda x: x[1], reverse=True)
 
-# Make a table with ones where jounal has field
+# Make a table with ones where jounal has a given field
+# transpose for fields in rows and journals in columns
 fieldsDF = pd.DataFrame( dict([(k, pd.Series( [1]*len(v), index=v )) for k,v in fields.items()]) ).transpose()
 fieldsDF.fillna(value=0, inplace=True)
+# default argsort is ascending, so reversing
+fields_ordering = fieldsDF.sum(axis=1).argsort()[::-1]
 # print fieldsDF.sum(axis=1).sort_values(ascending=False)
-fieldsDF.to_csv(os.path.join(data_dir, 'fields.txt'), sep='\t', encoding='utf-8')
+fieldsDF.iloc[fields_ordering,:].to_csv(os.path.join(data_dir, 'fields.txt'), sep='\t', encoding='utf-8')
 
 print 'concatenating...'
 dfs = pd.concat(list_dfs, axis=0)
+# Concatenated DataFrames index are screwed up, so reset to integers and delete old 'index'
+dfs.reset_index()
+dfs.drop('index', axis=1, inplace=True)
+
 print 'saving text output...'
-dfs.to_csv(os.path.join(data_dir, out_file), sep='\t', encoding='utf-8')
+dfs.iloc[:,fields_ordering].to_csv(os.path.join(data_dir, out_file_base + '.txt'), sep='\t', encoding='utf-8')
+
+# Note: de-dup while preserving nulls
+# http://stackoverflow.com/questions/23512339/drop-duplicates-while-preserving-nans-in-pandas
+# df[(~df.duplicated()) | (df['col'].isnull())]
+
+# Storing for easy loading in Pandas later
+# http://stackoverflow.com/questions/17098654/how-to-store-data-frame-using-pandas-python
+dfs.to_pickle(os.path.join(data_dir, out_file_base + '.pkl'))
+
+# To read
+# df = pd.read_pickle(os.path.join(data_dir, out_file_base + '.pkl'))
