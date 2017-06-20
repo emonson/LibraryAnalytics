504157be79b4b048b977df90d9668c883d3315f0
Eric E Monson <emonson@cs.duke.edu>
2017-06-20 15:23:35 -0400

    page views to CSV

--- GoogleAnalyticsQuickstart/DVSpageViews.py
+++ GoogleAnalyticsQuickstart/DVSpageViews.py
@@ -8,6 +8,8 @@ from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
 
+import pandas as pd
+
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
 CLIENT_SECRETS_PATH = 'client_secrets.json' # Path to client_secrets.json file.
@@ -54,9 +56,9 @@ def get_report(analytics):
         {
           'viewId': VIEW_ID,
           'pageSize': 10000,
-          'dateRanges': [{'startDate': '2014-07-20', 'endDate': 'today'}],
+          'dateRanges': [{'startDate': '2016-07-20', 'endDate': 'today'}],
           'metrics': [{'expression': 'ga:pageviews'}],
-          "dimensions": [{"name": "ga:pagePath"}],
+          "dimensions": [{"name": "ga:pagePath"}, {"name": "ga:pageTitle"}],
           "dimensionFilterClauses": [
             {
               "operator": "AND",
@@ -91,7 +93,10 @@ def print_response(response):
   for report in response.get('reports', []):
     columnHeader = report.get('columnHeader', {})
     dimensionHeaders = columnHeader.get('dimensions', [])
+    print dimensionHeaders
     metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+    print metricHeaders
+    print
     rows = report.get('data', {}).get('rows', [])
 
     for row in rows:
@@ -108,11 +113,46 @@ def print_response(response):
       print
     print "Total rows returned:", len(rows)
     
+def gather_table(response):
+  """Parses the Analytics Reporting API V4 response
+     and creates a list of dicts which will be put into a pandas DataFrame
+  """
+  # NOTE: Probably not dealing with multiple "reports" properly right now...
+  rows_list = []
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    dimensionHeaders = columnHeader.get('dimensions', [])
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+    rows = report.get('data', {}).get('rows', [])
+
+    for row in rows:
+      row_dict = {}
+      dimensions = row.get('dimensions', [])
+      dateRangeValues = row.get('metrics', [])
+      num_date_ranges = len(dateRangeValues)
+
+      for header, dimension in zip(dimensionHeaders, dimensions):
+        row_dict[header[3:]] = dimension
+
+      for i, values in enumerate(dateRangeValues):
+        # print 'Date range (' + str(i) + ')'
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          if num_date_ranges == 1:
+            row_dict[metricHeader.get('name')[3:]] = value
+          else:
+            row_dict[metricHeader.get('name')[3:]+'_'+str(i)] = value
+      
+      rows_list.append(row_dict)
+
+    return pd.DataFrame(rows_list)
+    
 def main():
 
   analytics = initialize_analyticsreporting()
   response = get_report(analytics)
-  print_response(response)
+  # print_response(response)
+  response_table = gather_table(response)
+  response_table.to_csv('page_views_DVS.csv', encoding='utf-8', index=False)
 
 if __name__ == '__main__':
   main()
